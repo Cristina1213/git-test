4a0c7985873d8346bfe22f3e9c41bcfed7885949
wy <3153426747@qq.com>
2022-04-11 14:39:10 +0800

    hotfix second commit

--- src/main/java/com/first/git/GitTest.java
+++ src/main/java/com/first/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello,world");
         System.out.println("hello,world,java");
         System.out.println("hello,world,hotfix");
+        System.out.println("hello,world,hotfix-text");
 
 
     }
