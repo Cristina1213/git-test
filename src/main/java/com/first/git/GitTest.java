0d05db8f980e913c6dad2845dec2aba2bb70d3b1
wy <3153426747@qq.com>
2022-04-11 13:09:49 +0800

    first commit

---
+++ src/main/java/com/first/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.first.git;
+
+/**
+ * @author kk
+ * @create 2022-04-2022/4/11 12:59
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
